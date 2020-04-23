db2fb7dedb80d4eeb9f889955e8404593d6c6caa
Mohammad Manzoor Alam <mohammad.manzoor8972@gmail.com>
2020-04-23 09:12:32 +0530

    added styled Component
    added HOF

--- src/Like.jsx
+++ src/Like.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+
+const Styled = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+  margin: 10px;
+  padding: 10px;
+`;
 
 const Like = () => {
   const [counter, setCounter] = React.useState(() => {
@@ -8,17 +17,30 @@ const Like = () => {
     return 0;
   });
   return (
-    <button
-      onClick={() => {
-        setCounter(counter + 1);
-        if (counter % 10 === 0) {
-          setCounterD(counter);
-        }
-      }}
-    >
-      ♥ {counterD}
-    </button>
+    <>
+      Like
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+          if (counter % 10 === 0) {
+            setCounterD(counter);
+          }
+        }}
+      >
+        ♥ {counterD}
+      </button>
+    </>
   );
 };
 
-export default Like;
+export default withStyledComponent(Like);
+
+function withStyledComponent(Component) {
+  return () => {
+    return (
+      <Styled>
+        <Component />
+      </Styled>
+    );
+  };
+}
